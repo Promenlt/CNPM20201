af6eb5853275b19ce252f69b530b9213c80f4631
Promenlt <68494848+Promenlt@users.noreply.github.com>
2020-10-15 13:00:36 +0700

    +Add homework 2:Lab 02+03

---
+++ Homework2/Lab03/PhpExcercise1/SquareCube.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Square and Cube</title>
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet">
+</head>
+    <font size="5" color="blue">
+        Generate Square and Cube Values</font>
+        <body>
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="GET">
+            <?php
+            if(array_key_exists("start", $_GET)){
+                        $start = $_GET["start"];
+                        $end = $_GET["end"];
+            } else {
+                $start = 0; $end =0;
+            }
+            ?>
+            <table>
+                <tr>
+                    <td>Select Start Number</td>
+                    <td><select name="start">
+                            <?php
+                            for($i=0;$i<=10;$i++){
+                                print("<option>$i</option>");
+                            }
+                            ?>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Select End Number:</td>
+                            <td><Select name="end">
+                        <?php
+                        for($i=0;$i<=20;$i++){
+                            if($end==$i){
+                                print("<option selected>$i</option>");
+                            }else{
+                                print ("<option>$i</option>");
+                            }
+                         
+                        }
+                        ?>
+                        </Select>
+                            </td>
+                </tr>
+                <tr>
+                    <td align="right"><input TYPE="SUBMIT" VALUE="Submit"></td>
+                    <td align="left"><input TYPE="RESET" VALUE="Reset"></td>
+                </tr>
+            </table>
+            <table>
+             
+                    <?php
+                    if(array_key_exists("start", $_GET)){
+                        print("<tr><th>Number</th><th>Square</th><th>Cube</th><tr>");
+                        
+                        $i = $start;
+                        while($i <= $end){
+                            $sqr=$i*$i;
+                            $cubed=$i*$i*$i;
+                            print "<TR><TD>$i</TD><TD>$sqr</TD><TD>$cubed</TD><TD>";
+                            $i=$i+1;
+                        }
+                    }
+                    ?>
+            </table>
+        </form>
+        </body>
+</html>
+    
